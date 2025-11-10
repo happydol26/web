19493dde3f94b90b2416419eb57005bb14201c34
happydol26 <h098s02316@chunchon.gwe.hs.kr>
2025-11-10 10:21:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -195,7 +195,7 @@ with col1:
     st.image("https://www.streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png", caption="Streamlit 로고")
 
 with col2:
-    st.write("유튜브 동영상 예제")
+    st.write("https://www.youtube.com/watch?v=qrqHlgqNTHo")
     st.video("https://www.youtube.com/watch?v=B2iAodr0fOo")
 
 # --- 10. 마무리 ---
